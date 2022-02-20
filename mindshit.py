228f78c8759e7a80cb2f5e62332f1398b2d39997
Hayden <asianhaydenxd@gmail.com>
2022-02-20 16:18:21 -0500

    Implement not equal comparison operator

--- mindshit.py
+++ mindshit.py
@@ -816,7 +816,27 @@ class Compiler:
                 result += self.visit(node.right)
                 right = self.pointer
                 
-                result += self.bf_parse('t0[-]r[-]x[r+t0+x-]t0[x+t0-]rt1[-]r[t1+r-]+y[t1-t0+y-]t0[y+t0-]t1[r-t1[-]]r',
+                result += self.bf_parse('t0[-]r[-]x[r+t0+x-]t0[x+t0-]t1[-]r[t1+r-]+y[t1-t0+y-]t0[y+t0-]t1[r-t1[-]]r',
+                    t0 = temp0,
+                    t1 = temp1,
+                    r  = returned,
+                    x  = left,
+                    y  = right,
+                )
+                
+                self.memory.rmv(temp0, temp1)
+                return result
+            
+            if node.token.full == (Tk.OP, '!='):
+                temp0, temp1, returned = self.memory.allocate(3)
+                
+                result += self.visit(node.left)
+                left = self.pointer
+                
+                result += self.visit(node.right)
+                right = self.pointer
+                
+                result += self.bf_parse('t0[-]r[-]x[r+t0+x-]t0[x+t0-]t1[-]r[t1+r-]y[t1-t0+y-]t0[y+t0-]t1[r+t1[-]]r',
                     t0 = temp0,
                     t1 = temp1,
                     r  = returned,
